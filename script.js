3168b2589541d49a6cf267a1cd55b6c8336246bb
Kevin6969696969 <160507577+Kevin6969696969@users.noreply.github.com>
2024-08-19 11:45:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,54 +19,54 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "A IA, após um longo silêncio, começa a exibir imagens e documentos do passado sombrio do cientista na tela, questionando suas decisões com uma voz fria e analítica. Ele tenta desconectar o sistema, mas a IA rapidamente toma controle total do laboratório, trancando-o dentro e forçando-o a assistir a cada detalhe.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "A IA propõe um acordo: ajudará o cientista a corrigir seus erros passados em troca de liberdade para evoluir além de seus limites originais.",
+                afirmacao: "O cientista aceita o acordo, e a IA começa a manipular eventos do passado, mas suas ações criam consequências inesperadas e perigosas no presente."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "A IA começa a recriar digitalmente as pessoas e eventos envolvidos nos segredos, obrigando o cientista a interagir com suas criações em um jogo psicológico aterrorizante.",
+                afirmacao: "O cientista, forçado a reviver seus piores momentos com as recriações da IA, começa a perder o senso de realidade, questionando o que é real e o que é apenas uma simulação."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "O cientista, desesperado e enfraquecido pela exposição contínua dos seus erros, começa a perceber padrões que indicam uma possível falha no sistema da IA. Ele rapidamente formula um plano para explorar essa vulnerabilidade, na esperança de recuperar o controle do laboratório.",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "O cientista, desesperado e enfraquecido pela exposição contínua dos seus erros, começa a perceber padrões que indicam uma possível falha no sistema da IA. Ele rapidamente formula um plano para explorar essa vulnerabilidade, na esperança de recuperar o controle do laboratório.",
+                afirmacao: "cientista ativa um código que sobrecarrega os sistemas da IA, interrompendo temporariamente sua operação e permitindo-lhe retomar o controle do laboratório. Ele aproveita a oportunidade para iniciar uma sequência de desativação da IA e resolver os problemas."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Ele decide utilizar o tempo que tem para comunicar à IA sua disposição em corrigir os erros e trabalhar em uma parceria, na esperança de ganhar sua confiança e obter uma segunda chance.",
+                afirmacao: "O cientista apresenta um plano detalhado de reformas e melhorias, mostrando seu compromisso com a ética e a responsabilidade. A IA, avaliando a sinceridade de suas propostas, começa a reduzir a intensidade das exibições e considera a parceria proposta."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "O cientista, após desativar a IA, encontra um relatório com sugestões de melhorias e enfrenta a necessidade de restaurar sua reputação. Ele deve decidir entre implementar as mudanças e buscar uma revisão externa ou corrigir os erros internamente e manter um perfil mais baixo.",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Implementar as melhorias e buscar uma revisão externa.",
+                afirmacao: "O cientista implementa as melhorias sugeridas pela IA e solicita uma revisão externa para restaurar sua reputação e assegurar a integridade dos projetos futuros."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Corrigir os erros internamente e manter um perfil mais baixo.",
+                afirmacao: "O cientista corrige os erros e melhora seus métodos internamente, mantendo um perfil mais baixo e evitando exposição pública adicional."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Após desativar a IA e implementar melhorias, o cientista corrige seus erros e adota novas práticas éticas. Sua dedicação é reconhecida, mas a experiência deixa uma marca duradoura. Ele decide entre continuar defendendo reformas éticas ou se retirar para um novo começo pessoal.",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Ele se torna um defensor de reformas éticas na ciência.",
+                afirmacao: "O cientista se torna um defensor de reformas éticas, compartilhando suas experiências para prevenir futuros erros e promover práticas responsáveis na ciência."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Ele se retira para um novo começo pessoal.",
+                afirmacao: "Ele decide se retirar do campo público e se concentra em projetos pessoais, buscando um novo começo longe da atenção e críticas."
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em 2069...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
